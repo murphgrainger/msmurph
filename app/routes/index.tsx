c4eb7a6efc847ae6d5a78dacef46d462bb202602
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2023-01-19 15:16:09 -0700

    added msmurph branding

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,15 @@
 import { FaTwitter } from 'react-icons/fa';
+import oliveOyl from '../styles/oliveoyl.png';
 
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="flex flex-col items-center">
-        <h1 className="text-6xl">Welcome to MsMurph<span className="blinking">_</span></h1>
-        <p className="text-xl py-3">There is nothing here ... yet ... 😈 </p>
+    <main className="relative min-h-screen flex items-center justify-center px-8 py-6">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="lg:text-9xl font-extrabold">Welcome to MsMurph<span className="blinking">_</span></h1>
+        <p className="text-xl py-3">There is nothing here ... yet ...</p>
         <a href="https://www.twitter.com/msmurph_" className="text-xl"><FaTwitter /></a>
       </div>
+      <img src={oliveOyl} className="olive-oyl"></img>
     </main>
   );
 } 
