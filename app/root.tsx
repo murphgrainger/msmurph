5580439f68098afa98a7f3851683276f96c65c4a
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2023-01-19 18:27:40 -0700

    added google tag manager

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,8 @@ import {
 } from "@remix-run/react";
 
 import styles from "./styles/tailwind.css";
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -22,6 +24,13 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function App() {
+  
+  useEffect(() => {
+    if(window!.location!.hostname!.includes('msmurph')) {
+      TagManager.initialize({gtmId: 'GTM-MKRQGLG'})
+    }
+    }, [])
+  
   return (
     <html lang="en" className="h-full">
       <head>
